f47c5f7d1e84eac985f3ac18979cb606cbb4bdd5
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-10-06 21:18:19 +0200

    Add more assets to be generated

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const sharp = require('sharp'), fs = require('fs')
+const fs = require('fs'), sharp = require('sharp'), toIco = require('to-ico')
+
+const APPLE_TOUCH_ICON_SIZE = 180
 
 const files = [
   [48, 'mipmap-mdpi/', 'ic_launcher.png'],
@@ -7,7 +9,8 @@ const files = [
   [144, 'mipmap-xxhdpi/','ic_launcher.png'],
   [192, 'mipmap-xxxhdpi/','ic_launcher.png'],
   [384, 'mipmap-xxxxhdpi/','ic_launcher.png'],
-  [512, '','logo.png']
+  [512, '','logo.png'],
+  [256, 'temp/','256.png']
 ]
 
 const width = 1024
@@ -19,7 +22,54 @@ if (!fs.existsSync('input.png')) {
   process.exit(1)
 }
 
+let innerSize = 0
+let margin = 0
+
+function delay(t) {
+  return new Promise(resolve => {
+     setTimeout(resolve, t)
+  })
+}
+
 async function main() {
+  if (!fs.existsSync('./output/')) fs.mkdirSync('./output/')
+  if (!fs.existsSync('./output/pwa/')) fs.mkdirSync('./output/pwa/')
+  if (!fs.existsSync('./output/pwa/temp')) fs.mkdirSync('./output/pwa/temp')
+
+  // Preview
+  sharp('input.png')
+    .resize(256, 256, {
+        fit: sharp.fit.contain,
+        background: '#fff'
+    })
+    .flatten({ background: '#fff' })
+    .extend({
+      top: 187,
+      bottom: 187,
+      left: 472,
+      right: 472,
+      background: '#fff'
+    })
+    .png()
+    .toFile('./output/preview.jpg', (err, _) => {
+      if (err) console.error(err.message)
+      else console.log('Created image: 1200 x 630')
+    })
+
+  // Apple touch icon
+  sharp('input.png')
+    .resize(APPLE_TOUCH_ICON_SIZE, APPLE_TOUCH_ICON_SIZE, {
+        fit: sharp.fit.contain,
+        background: '#fff'
+    })
+    .flatten({ background: '#fff' })
+    .png()
+    .toFile('./output/apple-touch-icon.png', (err, _) => {
+      if (err) console.error(err.message)
+      else console.log('Created image: ' + APPLE_TOUCH_ICON_SIZE)
+    })
+
+  // PWA icons
   const baseImage = await sharp('input.png')
     .resize(width, width, {
         fit: sharp.fit.contain,
@@ -32,14 +82,11 @@ async function main() {
     }])
     .toBuffer()
 
-  if (!fs.existsSync('./output/')) fs.mkdirSync('./output/')
-
-  files.forEach(item => {
-    if (!fs.existsSync('./output/' + item[1])) fs.mkdirSync('./output/' + item[1])
-
-    const innerSize = Math.round(item[0] * .92 / 2) * 2
-    const margin = (item[0] - innerSize) / 2
-    sharp(baseImage)
+  for (item of files) {
+    if (!fs.existsSync('./output/pwa/' + item[1])) fs.mkdirSync('./output/pwa/' + item[1])
+    innerSize = Math.round(item[0] * .92 / 2) * 2
+    margin = (item[0] - innerSize) / 2
+    await sharp(baseImage)
       .resize(innerSize, innerSize)
       .extend({
         top: margin,
@@ -49,11 +96,19 @@ async function main() {
         background: { r: 0, g: 0, b: 0, alpha: 0 }
       })
       .png()
-      .toFile('./output/' + item[1] + item[2], (err, _) => {
+      .toFile('./output/pwa/' + item[1] + item[2], (err, info) => {
         if (err) console.error(err.message)
-        else console.log('Created image')
+        else console.log('Created image: ' + info.width)
       })
+  }
+
+  // Favicon
+  while (!fs.existsSync('./output/pwa/temp/256.png')) await delay(100)
+  toIco([fs.readFileSync('./output/pwa/temp/256.png')]).then(buf => {
+    fs.writeFileSync('./output/favicon.ico', buf)
+    console.log('Created image: 256')
   })
+  fs.rmdirSync('./output/pwa/temp', { recursive: true })
 }
 
 main()
